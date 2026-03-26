4dd8484837420cf95dbbea7ece6238338baf2868
J-Jairo-C-Ordoniez <cordobaojhonjairo21@gmail.com>
2026-03-26 00:10:51 -0500

    updated

--- src/app/api/catalog/variants/route.js
+++ src/app/api/catalog/variants/route.js
@@ -2,6 +2,10 @@ import { CatalogController } from '@/modules/catalog/controllers/catalog.control
 
 const controller = new CatalogController();
 
+export async function GET(req) {
+  return controller.getVariantById(req);
+}
+
 export async function POST(req) {
   return controller.createVariant(req);
 }
